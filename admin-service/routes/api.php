26a32b37c1860e6a81672764aa52e16378d6b96c
ThNam2310 <namkim102@gmail.com>
2025-12-26 13:16:00 +0700

    Sync admin-service from main

---
+++ admin-service/routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\ProductController;
+
+Route::prefix('admin')->group(function () {
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::get('/products/{id}', [ProductController::class, 'show']);
+    Route::get('/categories', [ProductController::class, 'getCategories']);
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+    Route::get('/product_images/{id}', [ProductController::class, 'getProductImages']);
+    Route::put('/products/{id}/model', [ProductController::class, 'updateModelPath']);
+    Route::post('/product_images', [ProductController::class, 'storeProductImage']);
+});
